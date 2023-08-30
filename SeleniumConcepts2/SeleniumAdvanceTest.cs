d1e36c86bd68a721ad727efc8cb8b764f5bd0264
Balaji <balaji0017@gmail.com>
2023-08-30 14:42:51 +0530

    unit test and upload

---
+++ SeleniumConcepts2/SeleniumAdvanceTest.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unisys.SeleniumConcepts2
+{
+    public class SeleniumAdvanceTest
+    {
+        [Test]
+        public void UploadTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(5);
+            driver.Url = "https://smallpdf.com/pdf-to-word";
+
+            driver.FindElement(By.XPath("//input[@type='file']")).SendKeys(@"C:\Mine\Balaji-Profile_2023_1.pdf");
+        }
+
+    }
+}
