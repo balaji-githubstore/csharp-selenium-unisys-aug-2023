6bff29679a560dd65c9a8ab3ab226b0abcfb47e6
Balaji <balaji0017@gmail.com>
2023-08-30 15:19:08 +0530

    actions class demo

--- SeleniumConcepts2/SeleniumAdvanceTest.cs
+++ SeleniumConcepts2/SeleniumAdvanceTest.cs
@@ -16,7 +16,7 @@ namespace Unisys.SeleniumConcepts2
         {
             IWebDriver driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(5);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             driver.Url = "https://smallpdf.com/pdf-to-word";
             //string file = "Balaji    Profile_2023_1.pdf";
             //driver.FindElement(By.XPath("//input[@type='file']")).SendKeys(@"C:\Mine\" + file);
@@ -50,11 +50,46 @@ namespace Unisys.SeleniumConcepts2
             actions.MoveToElement(driver.FindElement(By.XPath("//a[text()='Membership']"))).Perform();
 
             //mousehover on Become a become
+            actions.MoveToElement(driver.FindElement(By.XPath("//a[text()='Become a Member']"))).Perform();
 
             //click on Membership Benefits
             driver.FindElement(By.XPath("//a[text()='Membership Benefits']")).Click();
 
             //click to apply online element
+            //driver.FindElement(By.PartialLinkText("CLICK TO APPLY")).Click();
+
+            driver.FindElement(By.XPath("//a[text()='Click to Apply Online']")).Click();
+        }
+
+        [Test]
+        public void Actions3Test()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Url = "https://nasscom.in/";
+
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(driver.FindElement(By.XPath("//a[text()='Membership']"))).Pause(TimeSpan.FromSeconds(1))
+                .MoveToElement(driver.FindElement(By.XPath("//a[text()='Become a Member']")))
+                .Build().Perform();
+
+            driver.FindElement(By.XPath("//a[text()='Membership Benefits']")).Click();
+            driver.FindElement(By.XPath("//a[text()='Click to Apply Online']")).Click();
+        }
+
+        [Test]
+        public void Action4Keyboard3Test()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Url = "https://www.google.com/";
+
+            Actions actions = new Actions(driver);
+            actions.KeyDown(Keys.Shift).SendKeys("hello").KeyUp(Keys.Shift).Pause(TimeSpan.FromSeconds(1))
+                .SendKeys(Keys.ArrowDown).SendKeys(Keys.ArrowDown).SendKeys(Keys.ArrowDown).Pause(TimeSpan.FromSeconds(1))
+                 .SendKeys(Keys.Enter).Build().Perform();
         }
     }
 }
